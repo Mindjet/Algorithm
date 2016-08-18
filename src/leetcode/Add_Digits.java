918ad343343fdf96df8c8d17ed8a226784777873
Mindjet <578746875@qq.com>
2016-08-18 20:55:36 +0800

    unknown changes

--- src/leetcode/Add_Digits.java
+++ src/leetcode/Add_Digits.java
@@ -3,46 +3,42 @@ package leetcode;
 public class Add_Digits {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println(addDigits(199));
 
 	}
-	
-	private static int addDigits(int num){
-		
+
+	private static int addDigits(int num) {
+
 		// Method 1
-//		String s = String.valueOf(num);
-//		char c[] = s.toCharArray();
-//		int sum = 0;
-//		
-//		while(c.length>1){
-//			
-//			sum = 0;
-//			for (int i = 0; i < c.length; i++) {
-//				sum+=Integer.parseInt(c[i]+"");
-//			}
-//			
-//			c = String.valueOf(sum).toCharArray();
-//			
-//		}
-//		
-//		return sum;
-		
+		// String s = String.valueOf(num);
+		// char c[] = s.toCharArray();
+		// int sum = 0;
+		//
+		// while(c.length>1){
+		//
+		// sum = 0;
+		// for (int i = 0; i < c.length; i++) {
+		// sum+=Integer.parseInt(c[i]+"");
+		// }
+		//
+		// c = String.valueOf(sum).toCharArray();
+		//
+		// }
+		//
+		// return sum;
+
 		/**
-		 *  Method 2
-		 *  Time Complexity = O(1)
-		 *  we can see the regular pattern is:
-		 *  num:	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21...
-		 *  ret:	0	1	2	3	4	5	6	7	8	9	1	2	3	4	5	6	7	8	9	1	2	3....
-		 *  
-		 *  I must say that it is freaking brilliant;
-		 *  
+		 * Method 2 Time Complexity = O(1) we can see the regular pattern is:
+		 * num: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21... ret:
+		 * 0 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3....
+		 * 
+		 * I must say that it is freaking brilliant;
+		 * 
 		 */
-		
-		return (num-1)%9+1;
-		
-		
+
+		return (num - 1) % 9 + 1;
+
 	}
 
 }
--- src/leetcode/Invert_Binary_Tree.java
+++ src/leetcode/Invert_Binary_Tree.java
@@ -5,11 +5,18 @@ import leetcode.Node.TreeNode;
 /**
  * Invert a binary tree.
  * 
- * 4 / \ 2 7 / \ / \ 1 3 6 9
- * 
- * to
- * 
- * 4 / \ 7 2 / \ / \ 9 6 3 1
+	     4
+	   /   \
+	  2     7
+	 / \   / \
+	1   3 6   9
+	
+	to
+	     4
+	   /   \
+	  7     2
+	 / \   / \
+	9   6 3   1
  * 
  * 
  */
