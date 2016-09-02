118f4f1a394224e5de209986e8d3af7f9314fc7d
Mindjet <578746875@qq.com>
2016-09-02 11:44:49 +0800

    update comment

--- src/leetcode/Single_Number.java
+++ src/leetcode/Single_Number.java
@@ -1,10 +1,9 @@
 package leetcode;
 
-
 /**
  * Given an array of integers, every element appears twice except for one. Find that single one.
  * 
- * Note:
+ * Note: 
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
  *
  */
@@ -12,38 +11,38 @@ package leetcode;
 public class Single_Number {
 
 	public static void main(String[] args) {
-		
-		int nums[] = {1,2,2,13,1};
+
+		int nums[] = { 1, 2, 2, 13, 1 };
 		System.out.println(singleNumber(nums));
 
 	}
-	
+
 	/**
-	 *	As the O(n) time complexity and the O(1) space complexity are required, 
-	 *  we are supposed to use one loop without nested loop, and the varaibles we create must be limited.
+	 * As the O(n) time complexity and the O(1) space complexity are required,
+	 * we are supposed to use one loop without nested loop, and the varaibles we create must be limited.
 	 */
-	
-	public static int singleNumber(int []nums){
-		
-		if(nums.length==0||nums==null){
-			
+
+	public static int singleNumber(int[] nums) {
+
+		if (nums.length == 0 || nums == null) {
+
 			return 0;
-			
+
 		}
-		
-		//Note that if a==b,then a^b==0. for example, a=1001 and b=1001, then a^b=0000.
-		//And a^b=b^a, it means that we can make zero when there are numbers that appear twice.
-		//Finally, note that 0^a=a, we can get the single number by XORing all elements.
-		
+
+		// Note that if a==b,then a^b==0. for example, a=1001 and b=1001, then a^b=0000.
+		// And with a^b=b^a, we can always make zero when there are numbers that appear twice.
+		// Finally, note that 0^a=a, we can get the single number by XORing all elements.
+
 		int result = nums[0];
 		for (int i = 1; i < nums.length; i++) {
-			
-			result = result^nums[i];
-			
+
+			result = result ^ nums[i];
+
 		}
-		
+
 		return result;
-		
+
 	}
 
 }
