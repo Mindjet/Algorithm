c5b8bf448ef9377bf24722eb4d9e01d1e7debe1b
e <578746875@qq.com>
2016-09-05 18:30:13 +0800

    create (power_of_three.java)

---
+++ src/leetcode/Power_of_Three.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Power_of_Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static boolean isPowerOfThree(int n){
+		
+		
+		
+		
+		return true;
+	}
+
+}
