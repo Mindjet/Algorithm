f516db34b1ef459d3056c1d6f19afb02e8d2a85d
Mindjet <578746875@qq.com>
2016-09-08 00:21:59 +0800

    create (remove_duplicates_from_sorted_list.java)

--- src/leetcode/Remove_Duplicates_From_Sorted_List.java
+++ src/leetcode/Remove_Duplicates_From_Sorted_List.java
@@ -3,11 +3,20 @@ package leetcode;
 import node.ListNode;
 import node.ListNode.ListNodeUtils;
 
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * 
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ *
+ */
+
 public class Remove_Duplicates_From_Sorted_List {
 
 	public static void main(String[] args) {
 
-		int[] datas = { 1, 1, 2, 1, 2, 3 };
+		int[] datas = { 1, 1, 1, 2, 2, 3, 3, 1, 1 };
 		ListNode head = ListNodeUtils.generateLinkedList(datas);
 		ListNodeUtils.displayLinkedList(head);
 		ListNodeUtils.displayLinkedList(deleteDuplicates(head));
@@ -20,23 +29,28 @@ public class Remove_Duplicates_From_Sorted_List {
 			return head;
 		}
 
-		ListNode reserved_head = head;
+		ListNode pre = head;
+		ListNode rear = head.next;
 
-		while (head != null) {
+		while (rear != null) {
+			
+			//If the pre's value equals to the rear's value,
+			//let the pre'next be the rear's next and do not move the pre. 
+			if (pre.val == rear.val)	
+				pre.next = rear.next;
+			
+			//If the pre's value is not the same as the its next value, move the pre to the next.
+			else if (pre.val != pre.next.val)
+				pre = pre.next;
 
-			if (head.next.val == head.val) {
-
-				if (head.next == null)
-					break;
-				else
-					head.next = head.next.next;
-			}
-
-			head = head.next;
+			if (rear.next == null)
+				break;
+			else
+				rear = rear.next;
 
 		}
 
-		return reserved_head;
+		return head;
 	}
 
 }
